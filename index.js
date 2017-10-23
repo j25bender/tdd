5e7a371fe4608795275aac5a2f1c947f27a27599
Jeff Bender <j25bender@gmail.com>
2017-10-23 14:23:42 -0600

    added scripts to filepath

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-	const randomNumberArray = require('./randomNumberArray.js');
-const bubbleSort = require('./bubbleSort.js');
-const insertionSort = require('./insertionSort.js');
-const mergeSort = require('./mergeSort.js');
-const quickSort = require ('./quickSort.js');
+const randomNumberArray = require('./scripts/randomNumberArray.js');
+const bubbleSort = require('./scripts/bubbleSort.js');
+const insertionSort = require('./scripts/insertionSort.js');
+const mergeSort = require('./scripts/mergeSort.js');
+const quickSort = require ('./scripts/quickSort.js');
 
 module.exports = {
 	randomNumberArray,
