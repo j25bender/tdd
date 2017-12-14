bfa4210531705ff6aa998f6142f98eaad55e1ab4
Jeff Bender <j25bender@gmail.com>
2017-12-14 00:13:06 -0700

    heap sort

---
+++ scripts/heapSort.js
@@ -0,0 +1,42 @@
+let lastIndex = arr.length - 1;
+
+const maxHeap = (arr, i) => {
+  let leftChild = 2 * i + 1;
+  let rightChild = 2 * i + 2; 
+  let parent = i;
+
+  if (leftChild < lastIndex && arr[leftChild] > arr[parent]){
+      parent = leftChild;
+  }
+  if (rightChild < lastIndex && arr[rightChild] > arr[parent]){
+      parent = rightChild;
+  }
+    
+  if (parent != i) {
+    let temp = arr[i];
+
+    arr[i] = arr[parent];
+    arr[parent] = temp;
+    maxHeap(arr, parent);
+  }
+}
+
+const sortHeap = arr => {
+  lastIndex = arr.length;
+
+  for(let i = Math.floor(lastIndex / 2); i >= 0; i--){
+    maxHeap(arr, i);
+  }
+  
+  for(i = arr.length - 1; i > 0; i--){
+    let temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      
+      lastIndex--;
+      maxHeap(arr, 0);
+    }
+    return arr;
+}
+
+module.exports = heapSort;
---
+++ tests/heapSort-test.js
@@ -0,0 +1,41 @@
+import { assert } from 'chai';
+const heapSort = require('../scripts/heapSort.js');
+const { populateRandomArray, sortCheck } = require('../scripts/randomNumberArray.js');
+
+describe('Heap Sort Test', () => {
+  
+    it('should be a function', () => {
+        assert.isFunction(maxHeap);
+        assert.isFunction(sortHeap);
+    });
+
+    it('should sort an array with 10 items', () => {
+        let tenOne = populateRandomArray(0, 100, 10);
+        assert.equal(sortCheck(heapSort(tenOne)),true);
+    });
+
+    it('should sort an array with 100 items', () => {
+        let tenTwo = populateRandomArray(0, 1000, 100);
+        assert.equal(sortCheck(heapSort(tenTwo)),true);
+    });
+
+    it('should sort an array with 1,000 items', () => {
+        let tenThree = populateRandomArray(0, 10000, 1000);
+        assert.equal(sortCheck(heapSort(tenThree)),true);
+    }); 
+
+    it('should sort an array with 10,000 items', () => {
+        let tenFour = populateRandomArray(0, 100000, 10000);
+        assert.equal(sortCheck(heapSort(tenFour)),true);
+    });
+
+    it('should sort an array with negative numbers', () => {
+        let negativeArray = populateRandomArray(-100, 1000, 100);
+        assert.equal(sortCheck(heapSort(negativeArray)),true);
+    });
+
+    it('should sort an array with letters', () => {
+        let lettersArray = ['g', 'n', 'b', 'e', 'r', 'y', 'z', 't', 'a', 'm'];
+        assert.deepEqual(heapSort(lettersArray), ['a', 'b', 'e', 'g', 'm', 'n', 'r', 't', 'y', 'z'])
+    });
+});
